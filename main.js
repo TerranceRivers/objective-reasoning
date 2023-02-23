0112f7a811ef4d5255a36afcbc19a8100014a67a
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-02-22 20:38:14 -0500

    new

--- main.js
+++ main.js
@@ -1,7 +1,53 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(object){
+    if (object.userRole === "ADMIN"){
+        return true
+    }else {
+        return false
+    }
+}
 
+function getEmail(object){
+let firstName = object.firstName.toLowerCase()
+let lastName = object.lastName.toLowerCase()
+return firstName + "." + lastName + "@codeimmersives.com"
+}
+
+function getPlaylistLength(playlist){
+return playlist.songs.length
+}
+
+function getHardestHomework(homeworks) {
+    if (homeworks.length === 0) {
+      return '';
+    }
+    
+    let hardest = homeworks[0];
+    for (let i = 1; i < homeworks.length; i++) {
+      if (homeworks[i].averageScore < hardest.averageScore) {
+        hardest = homeworks[i];
+      }
+    }
+    
+    return hardest.name;
+  }
+  
+  
+  
+  
+      
+      function createPhonebook(names, numbers) {
+        let phonebook = {};
+      
+        for (let i = 0; i < names.length; i++) {
+          phonebook[names[i]] = numbers[i];
+        }
+      
+        return phonebook;
+      }
+      
 
 
 // ┌─────────────────────────────────────┐
